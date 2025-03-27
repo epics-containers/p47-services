9bc4ca98f0f63cce127d563d737a364e6a7926c2
Giles Knap <giles.knap@diamond.ac.uk>
2025-03-27 10:29:03 +0000

    fix false/true for autoaddrlist in pvagw

--- services/p47-gateways/config/start-pva.sh
+++ services/p47-gateways/config/start-pva.sh
@@ -5,10 +5,14 @@
 # EPICS_PVA_AUTO_ADDR_LIST=YES implies we are running in hostNetwork
 #   in this case we can broadcast to find local subnet PVs
 
+THIS=$(realpath $(dirname $0))
+
 # IP lists for IOCS (blank if get_ioc_ips.py fails)
 if [[ ${HOST_NETWORK} == "NO" ]]; then
   export IPS="$(python3 /config/get_ioc_ips.py)"
   export EPICS_PVA_ADDR_LIST=${IPS:-127.0.0.1}
+else
+  export EPICS_PVA_ADDR_LIST=127.0.0.1
 fi
 
 # PORTS for CA and PVA
@@ -19,12 +23,19 @@ export PVA_SERVER_PORT=${PVA_SERVER_PORT:-5075}
 CA_DEBUG=${CA_DEBUG:-0}
 PVA_DEBUG=${PVA_DEBUG:-0}
 
+# AUTO_ADDR_LIST
+if [[ ${EPICS_PVA_ADDR_LIST} == "YES" ]]; then
+  export AUTO_ADDR_LIST=true
+else
+  export AUTO_ADDR_LIST=false
+fi
+
 # fix up the templated pva gateway config
-cat /config/pvagw.template |
+cat ${THIS}/pvagw.template |
   sed \
     -e "s/PVA_ADDR_LIST/${EPICS_PVA_ADDR_LIST}/" \
     -e "s/PVA_SERVER_PORT/${PVA_SERVER_PORT}/" \
-    -e "s/PVA_AUTO_ADDR_LIST/${EPICS_PVA_AUTO_ADDR_LIST}/" \
+    -e "s/PVA_AUTO_ADDR_LIST/${AUTO_ADDR_LIST}/" \
     > /tmp/pvagw.config
 
 # background the PVA Gateway
