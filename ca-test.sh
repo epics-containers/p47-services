7a899381d2597d70aeb7fe8595f756f16e006767
Giles Knap <giles.knap@diamond.ac.uk>
2025-07-25 13:47:04 +0100

    switch to using oci helm chart for gateways

--- ca-test.sh
+++ ca-test.sh
@@ -2,6 +2,8 @@
 
 # set up the environment for caget for this shell
 export EPICS_CA_NAME_SERVERS=localhost:9064
+# or do this with no ssh tunnel (but firewall issues may occur)
+# export EPICS_CA_NAME_SERVERS=172.23.168.161:9064
 export EPICS_CA_AUTO_ADDR_LIST=NO
 
 # demonstrate caget works
