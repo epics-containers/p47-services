7c427811114175a60988dde334764f95bc635705
Giles Knap <giles.knap@diamond.ac.uk>
2025-07-25 09:14:19 +0100

    final settings to make gateway work

---
+++ ca-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# set up the environment for caget for this shell
+export EPICS_CA_NAME_SERVERS=localhost:9064
+export EPICS_CA_AUTO_ADDR_LIST=NO
+
+# demonstrate caget works
+caget BL47P-EA-DCAM-01:UPTIME
+caget BL47P-EA-DET-01:DET:ArrayCounter_RBV
